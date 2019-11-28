02ae6c4d96be6d02a399014fe61a52ba90c07af1
Renato Teles <teles.ree@hotmail.com>
2019-11-27 22:57:02 -0200

    Date 02 Upado

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.DAY_OF_MONTH, 4);
+		cal.add(Calendar.YEAR, 2);
+		d = cal.getTime();
+		System.out.println(sdf.format(d));
+	}
+
+}
